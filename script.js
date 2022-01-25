d67585d3829f87765efd5a3c3c0eb79c198cddd8
kkrogs <kourtney.krogman@gmail.com>
2022-01-24 21:47:18 -0700

    Committing final version

--- script.js
+++ script.js
@@ -1,82 +1,72 @@
-//Creating and adding html
-//create an array of objects so that when you call the first object it populates your choices
-//Once I have an array of objects, then I can use the buttons to populate a h3 for the question and 4 buttons for the choices.
 
-
-///match dataset of what is clicked to the answer
-
-///////////////////////////////////////////////////////////
-
+// Increasing the current question index
 var currentQuestionIndex=0;
 
 //score variable can be defined here
 var score=0;
-//if it is correct, instead of doing an alert I would add a certain increment.
 
-
+// Questions and corresponding answers
 
 var question = [{question:"What tags do we used to create an ordered list?",answer:"ol,li",choices:["ul, li", "le,il","ol,li","li,li"]},{question:"Which coding language do we use to style a page in HTML?",answer:"CSS", choices:["Javascript","CSS", "HTML", "jQuery"]}, {question:"What coding language is used to create a web page?",answer:"HTML",choices:["HTML", "JQuery", "Javascript", "SQL"]}];
 
-//if currentquestionindex
+//variable that defines if the question is right or wrong
 
 var correct = ["Correct"];
- var incorrect = ["Incorrect"];
+var incorrect = ["Incorrect"];
 
-//var qNumber = 0; qNumber++;
-// var score = localStorage.getItem("score");
-
-
-
-//web apis activity 21 shows how to build this
-//scoreCount.textContent = score;
-
-
-//var question01 = document.getElementById("question01");
-
-
-// var q1ans1 = ["ul, li", "le, il", "ol, li", "li, li"];
-// var q2ans = ["Javascript", "CSS", "HTML", "jQuery"];
-// var q3ans = ["HTML", "JQuery", "Javascript", "SQL"];
-
+// Start section
 var starting = document.getElementById("start")
+// Question one display
 var firstQuestion = document.getElementById("question01")
+// Start button
 var startButton = document.getElementById("startButton")
+// Amount of seconds user has to complete the quiz
 var secondsLeft = 120;
+// References timer element in the document
 var timeEl = document.querySelector(".timer");
+// References 'question' section of the HTML
 var question001 = document.getElementById("text01");
+// First answer button
 var button01 = document.querySelector(".button1");
+//  Second answer button
 var button02 = document.querySelector(".button2");
+// 3rd answer button
 var button03 = document.querySelector(".button3");
+// 4th answer button
 var button04 = document.querySelector(".button4");
+// portion of html that displays submit and next buttons
 var correctIncorrect = document.querySelector(".correctOrIncorrect");
+// Next button
 var next = document.querySelector(".next");
+// Displays score text in the html
 var scoreCount = document.querySelector(".scorehtml");
-
-//new
+//submit button
 var finalScore = document.querySelector(".submitScore");
-
+// submit score option
 var showScorePage = document.getElementById("showScore");
+// Last page
 var finalDiv = document.getElementById("finalDiv");
-//new
 
+//submit score button
 var submitScoreBtn = document.querySelector(".submitScoreButton");
+// Final score text on the last page
 var finalScoreCt = document.querySelector(".highScore");
-
+// H2 in the score block
 var h2Score = document.querySelector(".scoreH2");
-var zeroSecs = document.querySelector(".zeroSeconds");
-
+// Timer H2 header
 var timerH2 = document.querySelector(".timerh2");
 
-// NEW JAVA
-var todoInput = document.querySelector("#todo-text");
-var todoForm = document.querySelector("#todo-form");
-var todoList = document.querySelector("#todo-list");
-var todoCountSpan = document.querySelector("#todo-count");
+// RENAME BELOW
 
-// NEW JAVA
+// var todoInput = document.querySelector("#todo-text");
+// var todoForm = document.querySelector("#todo-form");
+// var todoList = document.querySelector("#todo-list");
+// var todoCountSpan = document.querySelector("#todo-count");
 
+// var todos = [];
 
-
+// Hides start menu, displays first Question menu, and hides the final page when the 'start button' is clicked.
+// Also calls the display question function to display 
 startButton.addEventListener("click",function() {
 starting.hidden = true;
 firstQuestion.hidden = false;
@@ -86,6 +76,7 @@ displayQuestion();
 
 });
 
+// Makes the timer display and when the timer hits 0, makes it go to a blank page
 var timerInterval = setInterval(function() {
   secondsLeft--;
   timeEl.textContent = secondsLeft;
@@ -99,10 +90,7 @@ var timerInterval = setInterval(function() {
 
 }, 1000);
 
-//Make a function who's purpose is to display the next question
-
-
-
+//function who's purpose is to display the next question
 displayQuestion=function(){
 
   question001.textContent = question[currentQuestionIndex].question;
@@ -110,30 +98,18 @@ displayQuestion=function(){
   button02.textContent = question[currentQuestionIndex].choices[1];
   button03.textContent = question[currentQuestionIndex].choices[2];
   button04.textContent = question[currentQuestionIndex].choices[3];
-  //make value referencing part of the html that is hidden to unhide it, call the var here along with the current question index. Should also hide the questions
-// if (currentQuestionIndex > question.length) {
-// alert("test");
-// }
+
 
 
   console.log(currentQuestionIndex);
 }
+// When the 'next' button is clicked, display the next question by calling the displayQuestion function
 next.addEventListener("click",function(){
   currentQuestionIndex++;
-// if (currentQuestionIndex > question.length) 
-// { alert("abc");
-
-
-// }
-// else {
-//  alert("highscore";)
-//make high score section no longer hidden
-
 
 
 displayQuestion();
-//}
-//we were not incrementing correctly according to the web questions being displayed,why we had to switch it.Swapped line 90 with 91.
+
 
 });
 
@@ -141,7 +117,7 @@ displayQuestion();
 
 
 
-//end of function
+//Checks the anser to make sure it is correct. Then display the alert 'correct' or 'incorrect' based on whether or not the user chose the 'answer'
 
 checkAnswer=function() {
 if (question[currentQuestionIndex].answer===this.textContent)
@@ -170,14 +146,12 @@ button01.addEventListener("click",checkAnswer);
 
 
 
-
+// Clear the timer when it gets to the final score page
       finalScore.addEventListener("click",function(){
 //clear timer
 timerH2.hidden = true;
 clearInterval(timerInterval);
 
-
-//call the showing function instead of finalScore
 //if the user gets to this section, make the timer go to 0 seconds or clearInterval
 
 finalDiv.hidden = false;
@@ -193,22 +167,7 @@ event.preventDefault()
       });
 
 
-
-
-//new
-      // submitScoreBtn.addEventListener("click",function(event){
-
-
-       
-      //   // NEW.................................................
-      //   // event.preventDefault()
-      //   // finalScoreCt.textContent=score;
-        
-
-      //   // console.log(this.textContent);
-      //   // not added showfinalScore();
-      //          });
-//new
+// When the timer hits 0, go to a blank white page
 
 zeroSecsPage=function(){
   starting.hidden = true;
@@ -221,41 +180,35 @@ zeroSecsPage=function(){
 };
 
 
-//zeroSeconds
+var initialInput = document.querySelector("#initial-text");
+var initialForm = document.querySelector(".initial-form"); 
+var initialList = document.querySelector("#initial-list");
+var initialCountSpan = document.querySelector("#initial-count");
 
-// zeroSeconds.textContent;
-//   console.log(this.textContent);
+var initials = [];
 
-
-// NEW JAVA BELOW
-
-var todoInput = document.querySelector("#todo-text");
-var todoForm = document.querySelector(".todo-form");
-var todoList = document.querySelector("#todo-list");
-var todoCountSpan = document.querySelector("#todo-count");
-
-var todos = [];
 
 // The following function renders items in a todo list as <li> elements
-function renderTodos() {
+function renderInitials() {
   // Clear todoList element and update todoCountSpan
-  todoList.innerHTML = "";
-  todoCountSpan.textContent = todos.length;
+  initialList.innerHTML = "";
+  initialCountSpan.textContent = initials.length;
+  //find how much time is left and then that is what needs to be displayed in the todocount span. That is what will be pushed into my object that I will add into my local storage array.
 
   // Render a new li for each todo
-  for (var i = 0; i < todos.length; i++) {
-    var todo = todos[i];
+  for (var i = 0; i < initials.length; i++) {
+    var initial = initials[i];
 
     var li = document.createElement("li");
-    li.textContent = todo;
+    li.textContent = initial;
     li.setAttribute("data-index", i);
 
     var button = document.createElement("button");
     //adds final score to list
-    button.textContent = score;
+    button.textContent = score; 
 
     li.appendChild(button);
-    todoList.appendChild(li);
+    initialList.appendChild(li);
 
   
   }
@@ -264,54 +217,54 @@ function renderTodos() {
 // This function is being called below and will run when the page loads.
 function init() {
   // Get stored todos from localStorage
-  var storedTodos = JSON.parse(localStorage.getItem("todos"));
+  var storedInitials = JSON.parse(localStorage.getItem("initials"));
   // If todos were retrieved from localStorage, update the todos array to it
-  if (storedTodos !== null) {
-    todos = storedTodos;
+  if (storedInitials !== null) {
+    initials = storedInitials;
   }
 
   // This is a helper function that will render todos to the DOM
-  renderTodos();
+  renderInitials();
 }
 
-function storeTodos() {
+function storeInitials() {
   // Stringify and set key in localStorage to todos array
-  localStorage.setItem("todos", JSON.stringify(todos));
+  localStorage.setItem("initials", JSON.stringify(initials));
 }
 
 // Add submit event to form
-todoForm.addEventListener("submit", function(event) {
+initialForm.addEventListener("submit", function(event) {
   event.preventDefault();
 
-  var todoText = todoInput.value.trim();
+  var initialText = initialInput.value.trim();
 
   // Return from function early if submitted todoText is blank
-  if (todoText === "") {
+  if (initialText === "") {
     return;
   }
 
   // Add new todoText to todos array, clear the input
-  todos.push(todoText);
-  todoInput.value = "";
+  initials.push(initialText);
+  initialInput.value = "";
 
   // Store updated todos in localStorage, re-render the list
-  storeTodos();
-  renderTodos();
+  storeInitials();
+  renderInitials();
 });
 
 // Add click event to todoList element
-todoList.addEventListener("click", function(event) {
+initialList.addEventListener("click", function(event) {
   var element = event.target;
 
   // Checks if element is a button
   if (element.matches("button") === true) {
     // Get its data-index value and remove the todo element from the list
     var index = element.parentElement.getAttribute("data-index");
-    todos.splice(index, 1);
+    initials.splice(index, 1);
 
     // Store updated todos in localStorage, re-render the list
-    storeTodos();
-    renderTodos();
+    storeInitials();
+    renderInitials();
   }
 });
 
